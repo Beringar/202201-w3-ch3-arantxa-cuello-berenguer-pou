5c06d4b2d9db0cf808f9deda4cfbf91afb063f55
Arantxa Cuello <arantxa_312@hotmail.com>
2022-01-26 20:15:51 +0100

    rating component added

---
+++ RatingComponent.js
@@ -0,0 +1,38 @@
+import Component from "./Component.js";
+
+class RatingComponent extends Component {
+  constructor(parentElement, className, icon, actionOnClick) {
+    super(parentElement, className, "button");
+
+    this.generateHTML();
+
+    this.addListeners(actionOnClick);
+  }
+
+  generateHTML() {
+    this.element.innerHTML = ` 
+                <ul class="score">
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="1/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="2/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="3/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="4/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="5/5"></i>
+                  </li>
+                </ul>`;
+  }
+
+  addListeners(actionOnClick) {
+    this.element.addEventListener("click", actionOnClick);
+  }
+}
+
+export default RatingComponent;
--- SerieComponent.js
+++ SerieComponent.js
@@ -1,19 +1,28 @@
-import Component from "./"
+import Component from "./Component.js";
 
-class SerieComponent extends Component{
-  constructor(parentElement, htmlTag, actionOnClick,){
+class SerieComponent extends Component {
+  constructor(parentElement, htmlTag, actionOnClick) {
     super();
 
     this.actionOnClick = actionOnClick;
 
     this.generateHTML();
     this.addListeners();
-
   }
 
-  generateHTML(){
-    this.element.innerHTML =
-
+  generateHTML() {
+    this.element.innerHTML = ` 
+              <li class="serie">
+                <img
+                  class="serie__poster"
+                  src="img/${this.series.poster}"
+                  alt="The ${this.series.name} poster"
+                />
+                <h4 class="serie__title">${this.series.name}</h4>
+                <p class="serie__info">${this.series.creator}</p>
+               
+                <i class="fas fa-times-circle icon--delete"></i>
+              </li>`;
   }
 
   addListeners() {
