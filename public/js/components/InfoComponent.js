47d7a3352b3c11580a926bb5adff12649b9a6f62
Berenguer Pou <berenguer.pou@gmail.com>
2022-01-26 22:04:16 +0100

    defined InfoComponent and added to pageComponent

--- public/js/components/InfoComponent.js
+++ public/js/components/InfoComponent.js
@@ -1,14 +1,29 @@
 import Component from "./Component.js";
+import series from "../data/series.js";
 
 class InfoComponent extends Component {
-  constructor(parentElement, htmlTag) {
-    super(parentElement, htmlTag);
+  numberOfSeries;
 
-    this.generateHTML();
+  constructor(parentElement, htmlTag, watched) {
+    super(parentElement, htmlTag);
+    this.updatePendingSeries(watched);
+    this.generateHTML(watched);
   }
 
-  generateHTML() {
-    this.element.textContent = `You have watched  movies`;
+  generateHTML(watched) {
+    if (watched) {
+      this.element.innerHTML = `<h3 class="subsection-title">Watched series</h3>
+            <p class="info">You have watched ${this.numberOfSeries} series.</p>`;
+    } else {
+      this.element.innerHTML = `<h3 class="subsection-title">Pending series</h3>
+            <p class="info">You have ${this.numberOfSeries} series pending to watch.</p>`;
+    }
+  }
+
+  updatePendingSeries(watched) {
+    this.numberOfSeries = watched
+      ? series.filter((serie) => serie.watched).length
+      : series.filter((serie) => !serie.watched).length;
   }
 }
 
